a3e3f9a6a7b204024615ddb674afd020b5c1acc7
Arshadsahabdeen <arshadsahabdeen05@gmail.com>
2025-06-14 02:32:05 +0530

    app.mount changes

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from app.api.v1.endpoints import auth, material_master, material_process, stitching_details, tailor_master, dispatch, report
+from app.api.v1.endpoints import auth, material_master, material_process,  stitching_details, tailor_master, dispatch, report
 
 app = FastAPI()
 
-# Serve Angular static files under /static
-app.mount("/static", StaticFiles(directory="dist/login-frontend-v1/browser"), name="static")
+app.mount("/", StaticFiles(directory="dist/login-frontend-v1/browser", html=True), name="static")
 
-# CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,7 +14,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include your routers
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
 app.include_router(material_master.router, prefix="/api/v1/material_master", tags=["Material Master"])
 app.include_router(material_process.router, prefix="/material-process", tags=["Material Process"])
@@ -26,12 +22,6 @@ app.include_router(tailor_master.router, prefix="/tailor-master", tags=["Tailor 
 app.include_router(dispatch.router, prefix="/dispatch", tags=["Dispatch"])
 app.include_router(report.router, prefix="/report", tags=["Report"])
 
-# Serve index.html for root
 @app.get("/")
-def serve_root():
-    return FileResponse("dist/login-frontend-v1/browser/index.html")
-
-# Serve index.html for Angular client-side routes
-@app.get("/{full_path:path}")
-def serve_spa(full_path: str):
-    return FileResponse("dist/login-frontend-v1/browser/index.html")
+def root():
+    return {"message": "Welcome! Use POST /api/v1/auth/login to get token."}
