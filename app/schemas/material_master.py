093964f3de438fbb85712583c316dc4dd4bd3e88
Arshadsahabdeen <arshadsahabdeen05@gmail.com>
2025-06-14 23:14:50 +0530

    changes

--- app/schemas/material_master.py
+++ app/schemas/material_master.py
@@ -2,15 +2,17 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import date, datetime
 from decimal import Decimal
+from typing import Union
 
 class MaterialBase(BaseModel):
     Material_Desc: str
-    Quantity: Decimal
+    Quantity: Union[Decimal, float, int]
     Color: str
-    Price: Decimal
+    Price: Union[Decimal, float, int]
     Pattern: str
     Purchase_Date: date
     Comments: Optional[str]
+
 
 class MaterialCreate(MaterialBase):
     pass
