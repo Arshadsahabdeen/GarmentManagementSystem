db9b3c28155fa7703bdbcfc9ce00f9d5617b046a
Arshadsahabdeen <arshadsahabdeen05@gmail.com>
2025-06-14 18:01:01 +0530

    db changes

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost/garment_db"
+SQLALCHEMY_DATABASE_URL = (
+    "mysql+pymysql://uf8nqwceg7hen4ls:fABMZigVfzcUCaXG758Z@bh9pddrgykbwbuzywtdb-mysql.services.clever-cloud.com:3306/bh9pddrgykbwbuzywtdb"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
