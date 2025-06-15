006c3f420b415bd7a7671ae9d5a6f4cb1ef4be1d
Arshadsahabdeen <arshadsahabdeen05@gmail.com>
2025-06-15 13:43:19 +0530

    table name changes material_process

--- app/db/models/material_process.py
+++ app/db/models/material_process.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import func
 from app.database import Base
 
 class Material_Process(Base):
-    __tablename__ = "Material_Process"
+    __tablename__ = "material_process"
     Material_Process_Id = Column(Integer, primary_key=True, index=True)
     Material_Id = Column(Integer, ForeignKey("material_master.Material_Id"), nullable=False)
     Quantity_Processed = Column(DECIMAL(10, 2), nullable=False)
